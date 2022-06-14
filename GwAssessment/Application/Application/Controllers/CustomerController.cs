d046992be6ce6029049338b6fc5758bbbe228cef
smahapatra86 <smahapatra86@gmail.com>
2022-06-14 21:51:24 +0530

    Add skeleton

---
+++ GwAssessment/Application/Application/Controllers/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Under development");
+        }
+    }
+}
